94827eec662f87b0ee7e7ba30287cfe3e7ad7a12
hodisanmihai <150062479+hodisanmihai@users.noreply.github.com>
2024-02-10 23:11:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('yesBtn').addEventListener('click', function() {
+    alert('Asaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!');
+  });
+  
+  document.getElementById('noBtn').addEventListener('click', function() {
+    alert('Cum adica nu?');
+  });
+  
